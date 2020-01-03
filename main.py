cb0899acdbd3f7e79818bfb3788de02a72b996b8
yuman yin <yinyuman96@gmail.com>
2020-01-03 21:27:47 +0100

    添加主函数的注释

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import extractFrame
 
 # The directory of the folder where the video is located
 path = "/Users/wangwei/Documents/我爱学习 学习爱我/Project/2DMOT2015/train/TUD-Stadtmitte"
-listOfPic, numOfPic = extractFrame.readVideo(path, "video.mp4")    # Generate image arrays and position arrays
-listOfRec = extractFrame.readTxt(path, numOfPic)     # The number of images in the incoming path and video
 
-writeVideo.writeVideo(path, listOfPic, listOfRec, numOfPic)    #
+# Generate image arrays and position arrays
+listOfPic, numOfPic = extractFrame.readVideo(path, "video.mp4")
+
+# The number of images in the incoming path and video
+listOfRec = extractFrame.readTxt(path, numOfPic)
+
+'''
+1. Slice the rectangles
+2. Draw graphs
+3. Match the graphs
+4. Product a new video
+'''
+writeVideo.writeVideo(path, listOfPic, listOfRec, numOfPic)
 

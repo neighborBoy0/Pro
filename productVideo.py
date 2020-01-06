a9b110bf3b40f9563c69bb6b0c9f74207be09386
wangwei <epuwangwei@163.com>
2020-01-06 01:23:33 +0100

    修改了排序方法

--- productVideo.py
+++ productVideo.py
@@ -3,6 +3,20 @@ import cv2
 import time
 
 """
+* Objective: sort those files in path
+* input: path - work folder
+* output: a dictionary which saves those pictures sorted
+"""
+def sortFileName(path):
+    filelist = os.listdir(path)
+    names = {}
+    for item in filelist:
+        if item.endswith(".jpg"):
+            n = int(item[0:-4])
+            names[n] = item
+    return names
+
+"""
 * objective: generate video by pictures
 * input: path - pictures path
          size - size of picture and video
@@ -10,7 +24,7 @@ import time
 """
 def picvideo(path, size):
     # obtain all of files in the path
-    filelist = os.listdir(path)
+    #filelist = os.listdir(path)
 
     '''
     fps:
@@ -25,10 +39,12 @@ def picvideo(path, size):
 
     video = cv2.VideoWriter(file_path, fourcc, fps, size)
 
-    for item in sorted(filelist):
+    #for item in sorted(filelist):
+    fileDic = sortFileName(path)
+    for i in range(0, len(fileDic)):
         # decide the file is jpg or not
-        if item.endswith('.jpg'):
-            item = path + '/' + item
+        if fileDic[i].endswith('.jpg'):
+            item = path + '/' + fileDic[i]
 
             # Use opencv to read the image and directly return the numpy.ndarray object. The channel order is BGR. Note that it is BGR. The default channel value is 0-255.
             img = cv2.imread(item)
@@ -39,4 +55,4 @@ def picvideo(path, size):
     video.release()
 
 if __name__ == '__main__':
-    picvideo(r'/Users/wangwei/Documents/我爱学习 学习爱我/Project/2DMOT2015/train/TUD-Stadtmitte/graphs', (640, 480))
+    picvideo(r'/Users/wangwei/Documents/我爱学习 学习爱我/Project/2DMOT2015/train/TUD-Stadtmitte/imgTest', (640, 480))

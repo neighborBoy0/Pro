90a104263fb872c7bacf493f4972890f6c3f1a7d
wangwei <epuwangwei@163.com>
2020-01-06 01:23:03 +0100

    修改了入口

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import writeVideo
 import extractFrame
 
 # The directory of the folder where the video is located
-path = "/Users/wangwei/Documents/我爱学习 学习爱我/Project/2DMOT2015/train/TUD-Stadtmitte"
+path = input("Importer le chemin de travail complet:")
+videoName = input("Importer le nom de video:")
+#path = "/Users/wangwei/Documents/我爱学习 学习爱我/Project/2DMOT2015/train/TUD-Stadtmitte"
 
 # Generate image arrays and position arrays
-listOfPic, numOfPic = extractFrame.readVideo(path, "video.mp4")
+listOfPic, numOfPic = extractFrame.readVideo(path, videoName)
 
 # The number of images in the incoming path and video
 listOfRec = extractFrame.readTxt(path, numOfPic)
